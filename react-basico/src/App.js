a82d49918a583f638a5748956fcfb8bf3c043391
kalangoti <kalangoti@gmail.com>
2022-03-23 19:19:18 -0300

    added components

---
+++ react-basico/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import Comentario from "./components/Comentario";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu projeto</h1>
+      <Comentario />
+      <Comentario />
+      <Comentario />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-basico/src/components/Comentario.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Comentario.css";
+
+const Comentario = () => (
+  <div className="Comentario">
+    <div>João:</div>
+    <div>Olá eu sou um componente</div>
+  </div>
+);
+
+export default Comentario;
